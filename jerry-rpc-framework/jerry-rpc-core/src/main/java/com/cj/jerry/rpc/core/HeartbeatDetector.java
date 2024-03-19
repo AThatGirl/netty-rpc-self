54c090854c991ccf8cc31c4ad651ad9ad968c436
Jerry <Jerry@jerry.com>
2024-03-19 21:35:11 +0800

    9.1动态感知服务下线

--- jerry-rpc-framework/jerry-rpc-core/src/main/java/com/cj/jerry/rpc/core/HeartbeatDetector.java
+++ jerry-rpc-framework/jerry-rpc-core/src/main/java/com/cj/jerry/rpc/core/HeartbeatDetector.java
@@ -12,12 +12,11 @@ import io.netty.channel.ChannelFutureListener;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetSocketAddress;
-import java.util.List;
-import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Slf4j
 public class HeartbeatDetector {
@@ -55,44 +54,60 @@ public class HeartbeatDetector {
             //将缓存中所有的channel遍历
             Map<InetSocketAddress, Channel> channelCache = JerryRpcBootstrap.CHANNEL_CACHE;
             for (Map.Entry<InetSocketAddress, Channel> entry : channelCache.entrySet()) {
-                Channel channel = entry.getValue();
 
-                long start = System.currentTimeMillis();
 
-                JerryRpcRequest jerryRpcRequest = JerryRpcRequest.builder()
-                        .requestId(JerryRpcBootstrap.idGenerator.getId())
-                        .compressType(CompressorFactory.getCompressor(JerryRpcBootstrap.COMPRESS_TYPE).getCode())
-                        .requestType(RequestType.HEART_BEAT.getId())
-                        .serializeType(SerializerFactory.getSerializer(JerryRpcBootstrap.SERIALIZE_TYPE).getCode())
-                        .timeStamp(start)
-                        .build();
-                //写出报文
-                CompletableFuture<Object> completableFuture = new CompletableFuture<>();
-                JerryRpcBootstrap.PENDING_QUESTIONS.put(jerryRpcRequest.getRequestId(), completableFuture);
-                channel.writeAndFlush(jerryRpcRequest).addListener((ChannelFutureListener) promis -> {
-                    if (promis.isSuccess()) {
-                        completableFuture.complete(null);
-                    } else {
-                        completableFuture.completeExceptionally(promis.cause());
+                int tryTimes = 3;
+                while (tryTimes > 0) {
+
+                    Channel channel = entry.getValue();
+
+                    long start = System.currentTimeMillis();
+
+                    JerryRpcRequest jerryRpcRequest = JerryRpcRequest.builder()
+                            .requestId(JerryRpcBootstrap.idGenerator.getId())
+                            .compressType(CompressorFactory.getCompressor(JerryRpcBootstrap.COMPRESS_TYPE).getCode())
+                            .requestType(RequestType.HEART_BEAT.getId())
+                            .serializeType(SerializerFactory.getSerializer(JerryRpcBootstrap.SERIALIZE_TYPE).getCode())
+                            .timeStamp(start)
+                            .build();
+                    //写出报文
+                    CompletableFuture<Object> completableFuture = new CompletableFuture<>();
+                    JerryRpcBootstrap.PENDING_QUESTIONS.put(jerryRpcRequest.getRequestId(), completableFuture);
+                    channel.writeAndFlush(jerryRpcRequest).addListener((ChannelFutureListener) promis -> {
+                        if (promis.isSuccess()) {
+                            completableFuture.complete(null);
+                        } else {
+                            completableFuture.completeExceptionally(promis.cause());
+                        }
+                    });
+
+                    Long endTime = 0L;
+                    try {
+                        //get()会阻塞，方法如果回不来，会一直阻塞
+                        completableFuture.get(1, TimeUnit.SECONDS);
+                        endTime = System.currentTimeMillis();
+                    } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                        log.error("和地址为【{}】的主机连接异常, 正在进行第【{}】次尝试", channel.remoteAddress(), 4 - tryTimes);
+                        //重试次数-1
+                        tryTimes--;
+                        if (tryTimes == 0) {
+                            //将失效的地址移除
+                            JerryRpcBootstrap.CHANNEL_CACHE.remove(entry.getKey());
+                        }
+                        try {
+                            Thread.sleep(10 * (new Random().nextInt(5)));
+                        } catch (InterruptedException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                        continue;
                     }
-                });
 
-                Long endTime = 0L;
-                try {
-                    completableFuture.get();
-                    endTime = System.currentTimeMillis();
-                } catch (InterruptedException | ExecutionException e) {
-                    throw new RuntimeException(e);
+                    Long time = endTime - start;
+                    //使用TreeMap进行缓存
+                    JerryRpcBootstrap.ANSWER_TIME_CHANNEL_CACHE.put(time, channel);
+                    log.info("给【{}】发送心跳包，耗时：{}ms", entry.getKey(), time);
+                    break;
                 }
-
-                Long time = endTime - start;
-                //使用TreeMap进行缓存
-                JerryRpcBootstrap.ANSWER_TIME_CHANNEL_CACHE.put(time, channel);
-                log.info("给【{}】发送心跳包，耗时：{}ms", entry.getKey(), time);
-
-
-
-
             }
         }
     }
